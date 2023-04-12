2db5dbc6769d061335acf0afd5f0c0e798bfd0d6
Baurzhan Yussupov <baurzhangulnur@gmail.com>
2023-04-12 19:05:29 +0600

    #2 Added getting and validating props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="playingCards">
-        <Card />
+        <Card suit="D" rank="K" />
       </div>
     </div>
   );
--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,11 +1,39 @@
 import React from "react";
 import "../../assets/cards.css";
 
-const Card = () => {
+const Card = (props) => {
+    let suitName = "";
+    let suitSymbol = "";
+
+    switch (props.suit) {
+        case "H":
+            suitName = "hearts";
+            suitSymbol = "\u2665"; // ♥
+            break;
+
+        case "D":
+            suitName = "diams";
+            suitSymbol = "\u2666"; // ♦
+            break;
+
+        case "C":
+            suitName = "clubs";
+            suitSymbol = "\u2663"; // ♣
+            break;
+
+        case "S":
+            suitName = "spades";
+            suitSymbol = "\u2660"; // ♠
+            break;
+
+        default:
+            return null;
+    }
+
     return (
-        <div className="card rank-7 spades">
-            <span className="rank">7</span>
-            <span className="suit">&spades;</span>
+        <div className={`card rank-${props.rank.toLowerCase()} ${suitName}`}>
+            <span className="rank">{props.rank}</span>
+            <span className="suit">{suitSymbol}</span>
         </div>
     )
 }
